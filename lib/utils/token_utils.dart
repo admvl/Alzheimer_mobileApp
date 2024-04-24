03826a41ae06d67df3c89e4d28601013f6d071fa
luisrood <luisrood@icloud.com>
2024-04-24 02:25:50 -0600

    Mostrar información de usuario

---
+++ lib/utils/token_utils.dart
@@ -0,0 +1,20 @@
+import 'package:alzheimer_app1/services/usuarios_service.dart';
+import 'package:jwt_decode/jwt_decode.dart';
+
+class TokenUtils{
+  final UsuariosService usuariosService = new UsuariosService();
+  Future<String> getIdUsuarioToken() async{
+    String? token = await storage.read(key: 'token');
+    if(token!=null){
+      Map<String,dynamic> decodedToken = Jwt.parseJwt(token);
+      String? idUsuario = decodedToken['sub'];
+      if(idUsuario!=null){
+        return idUsuario;
+      }else{
+        throw Exception('El token no contiene un ID de usuario valido');
+      }
+    }else{
+      throw Exception('Token expirado');
+    }
+  }
+}
