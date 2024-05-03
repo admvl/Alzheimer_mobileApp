5b47f856895b0a113a6c1d0029d05fc6e80da6a0
luisrood <luisrood@icloud.com>
2024-05-03 02:08:15 -0600

    IPS cambiadas

--- lib/services/medicamentos_service.dart
+++ lib/services/medicamentos_service.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 import 'package:alzheimer_app1/models/medicamentos.dart';
 
 class MedicamentosService{
-  final String baseUrl = "http://192.168.68.125:7084/api";
+  final String baseUrl = "http://192.168.137.1:7084/api";
 
   MedicamentosService();
 
